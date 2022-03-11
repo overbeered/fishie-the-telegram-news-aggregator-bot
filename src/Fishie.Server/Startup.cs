2d9db5d4ebaf134409d5ff6e1e8337a6f028afcb
gena56rus1 <gena56rus1@gihub.com>
2022-03-11 12:47:21 +0300

    [1] fixed

--- src/Fishie.Server/Startup.cs
+++ src/Fishie.Server/Startup.cs
@@ -1,18 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace OverbeeredTgNewsBot.Server
+namespace Fishie.Server
 {
     public class Startup
     {
@@ -29,10 +22,9 @@ namespace OverbeeredTgNewsBot.Server
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "OverbeeredTgNewsBot.Server", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Fishie.Server", Version = "v1" });
             });
         }
-       
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -42,7 +34,7 @@ namespace OverbeeredTgNewsBot.Server
             }
 
             app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OverbeeredTgNewsBot.Server v1"));
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fishie.Server v1"));
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
